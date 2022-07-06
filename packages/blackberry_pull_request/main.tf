38146a5e1751ae34c2e6f1372ba536d47d3e87df
Ramesh Nakka <ramesh.nakka@sainsburys.co.uk>
2022-07-06 16:56:59 +0100

    for testing

---
+++ packages/blackberry_pull_request/main.tf
@@ -0,0 +1,77 @@
+
+
+  locals {
+
+  ou_config = yamldecode(file("Glue.yaml"))
+
+  expanded_names = flatten([
+      for key1, value1 in local.ou_config.AWS_GLUE_JOBS: [
+          for key2, value2 in value1: [
+              for key3, value3 in value2: [
+                  for key4, value4 in value3: [
+                      for key5, value5 in value4: [
+                          value5
+                      ]
+                  ]
+              ]
+          ]
+      ]
+  ])
+
+
+}
+
+module "blackberry" {
+    source = "../modules/blackberry_infra/glueresourcecreation"
+    create = var.create_connection
+    name = var.conn_name
+    url  = var.conn_url
+    user = var.conn_user
+    pass = var.conn_pass
+    sg_ids = var.conn_sg_ids
+    subnet = var.conn_subnet
+    azs    = var.conn_azs
+    type        = var.conn_type
+    catalog_id  = var.conn_catalog_id
+    description = var.conn_description
+    criteria    = var.conn_criteria
+}
+module "blackberryglues3"{
+    source = "../modules/blackberry_infra/glues3"
+    upload_obj_path = var.s3_upload_obj_path
+    #bucket_name_obj_upload="js-s3-aws-glue-bb"
+}
+module "blackberrysecretmanager" {
+    source = "../modules/blackberry_infra/gluescretmanager"
+    secret_manager_credentials = var.aws_secret_manager_credentials
+}
+module "blackberryterraform" {
+    #source                      = "../modules/blackberry_infra"
+    for_each = {for key, val in local.expanded_names: key => val}
+    source                      = "../modules/blackberry_infra/gluejobdeployment"
+    
+    s3_file_path                = "scripts/${each.value.File_name}"
+    bucket_name                 = each.value.Bucket_name
+    file_name                   = each.value.File_name
+    glue_job_name               = each.value.Name
+    glue_job_role_arn           = each.value.Role
+    glue_job_description        = each.value.Description
+    glue_job_connections        = each.value.Connections
+    glue_job_default_arguments  = each.value.DefaultArguments
+    glue_job_max_capacity       = each.value.MaxCapacity
+    glue_job_max_retries        = each.value.MaxRetries
+    glue_job_timeout            = each.value.Timeout
+    glue_job_execution_property = [
+        {
+            max_concurrent_runs  = var.max_concurrent_runs
+        }
+    ]
+
+    #account_number                       = var.account_number
+#   repo_name                            = var.repo_name
+#   environment                          = var.environment
+#   application_source_branch            = var.application_source_branch
+   
+
+
+}
---
+++ packages/modules/blackberry_infra/glues3/wheelfiles/glue_python_libs/etl_config.py
@@ -0,0 +1,9 @@
+
+BUCKET_NAME = 'aws-glue-target-poc'
+
+PARENT_OBJECT_FOLDER = 'r10region/data/'
+
+FILE_EXTENSION = '.txt.gz'
+
+FOLDER_DATE_FORMAT = "%Y%m%d"
+
