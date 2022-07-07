bd3fffcca04963b40bd27f612407354408463829
Ramesh Nakka <ramesh.nakka@sainsburys.co.uk>
2022-07-07 12:36:30 +0100

    for testing

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@
 
 module "blackberry" {
     #source = "git@github.com:rameshnakka778/githubworkflowterraform.git//packages/modules/blackberry_infra/gluesourcecreation"
-    source ="modules/blackberry_infra/glueresourcecreation"
+    source ="github.com/rameshnakka778/githubworkflowterraform.git//modules/blackberry_infra/glueresourcecreation?ref=main"
     create = var.create_connection
     name = var.conn_name
     url  = var.conn_url
@@ -39,17 +39,17 @@ module "blackberry" {
 }
 module "blackberryglues3"{
     #source = "git@github.com:rameshnakka778/githubworkflowterraform.git//packages/modules/blackberry_infra/glues3"
-    source = "modules/blackberry_infra/glues3"
+    source = "github.com/rameshnakka778/githubworkflowterraform.git//modules/blackberry_infra/glues3?ref=main"
     upload_obj_path = var.s3_upload_obj_path
     #bucket_name_obj_upload="js-s3-aws-glue-bb"
 }
 module "blackberrysecretmanager" {
     #source = "git@github.com:rameshnakka778/githubworkflowterraform.git//packages/modules/blackberry_infra/gluesecretmanager"
-    source = "modules/blackberry_infra/gluesecretmanager"
+    source = "github.com/rameshnakka778/githubworkflowterraform.git//modules/blackberry_infra/gluescretmanager?ref=main"
     secret_manager_credentials = var.aws_secret_manager_credentials
 }
 module "blackberryterraform" {
-    source                      = "modules/blackberry_infra/gluejobdeployment"
+    source                      = "github.com/rameshnakka778/githubworkflowterraform.git//modules/blackberry_infra/gluejobdeployment?ref=main"
     for_each = {for key, val in local.expanded_names: key => val}
     #source                      = "git@github.com:rameshnakka778/githubworkflowterraform.git//packages/modules/blackberry_infra/gluejobdeployment"
     #source              ="https://github.com/rameshnakka778/githubworkflowterraform.git//packages/modules/blackberry_infra/gluejobdeployment"
