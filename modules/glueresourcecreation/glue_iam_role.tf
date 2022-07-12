dc78cf1931d1c5e6e0ec387893c0922e6b213e1f
Ramesh Nakka <ramesh.nakka@sainsburys.co.uk>
2022-07-12 15:48:43 +0100

    for github work flows

--- modules/glueresourcecreation/glue_iam_role.tf
+++ modules/glueresourcecreation/glue_iam_role.tf
@@ -6,30 +6,30 @@ output "account_id" {
 resource "aws_iam_role" "js_dpp1_glue1" {
   name                 = "ramesh-${var.service_name}-${var.environment}-glue-service-role"
   permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/ccoe/js-developer"
-  assume_role_policy   = file("grada_product_nonprd_glue_service_role_test.json")
+  assume_role_policy   = file("./grada_product_nonprd_glue_service_role_test.json")
 }
 
 resource "aws_iam_role_policy" "js_dpp1_glue1_glue_policy" {
   name = "ramesh-${var.service_name}-${var.environment}-CloudWatchFullAccess"
   role = aws_iam_role.js_dpp1_glue1.id
-  policy =file("CloudWatchFullAccesspolicy.json.tpl")
+  policy =file("./CloudWatchFullAccesspolicy.json.tpl")
 }
 
 
 resource "aws_iam_role_policy" "js_dpp1_glue1_policy1" {
   name = "ramesh-${var.service_name}-${var.environment}-AWSGlueServiceRole"
   role = aws_iam_role.js_dpp1_glue1.id
-  policy =file("AWSGlueServiceRolepolicy.json.tpl")
+  policy =file("./AWSGlueServiceRolepolicy.json.tpl")
 }
 
 resource "aws_iam_role_policy" "js_dpp1_glue1_policy2" {
   name = "ramesh-${var.service_name}-${var.environment}-dpp_s3_read_access"
   role = aws_iam_role.js_dpp1_glue1.id
-  policy =file("grada_product_nonprod_s3_read_access.json.tpl")
+  policy =file("./grada_product_nonprod_s3_read_access.json.tpl")
 }
 
 resource "aws_iam_role_policy" "js_dpp1_glue1_policy3" {
   name = "ramesh-${var.service_name}-${var.environment}-SecretsManagerReadWrite"
   role = aws_iam_role.js_dpp1_glue1.id
-  policy =file("SecretsManagerReadWrite.json.tpl")
+  policy =file("./SecretsManagerReadWrite.json.tpl")
 }
