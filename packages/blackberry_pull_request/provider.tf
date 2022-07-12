1050a9644794a5c8121dfb52b94665bb5268c59f
Narayanam Desik <98107872+Narayanam-desik@users.noreply.github.com>
2022-07-12 11:53:52 +0100

    Update provider.tf

--- packages/blackberry_pull_request/provider.tf
+++ packages/blackberry_pull_request/provider.tf
@@ -18,6 +18,8 @@ terraform {
 provider "aws" {
     region = var.my_region
     #profile = var.my_profile
+    access_key = $AWS_ACCESS_KEY_ID
+    secret_key = $AWS_SECRET_ACCESS_KEY
 }
 # terraform {
   
